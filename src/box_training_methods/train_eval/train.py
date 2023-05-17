fe5538019ca46f81cba3cb2c1177fffe329e4304
Dhruvesh <4356534+dhruvdcoder@users.noreply.github.com>
2023-05-16 22:53:42 -0400

    Add wandb tags

--- src/box_training_methods/train_eval/train.py
+++ src/box_training_methods/train_eval/train.py
@@ -39,7 +39,8 @@ def training(config: Dict) -> None:
     """
 
     if config["wandb"]:
-        wandb.init(settings=wandb.Settings(start_method="fork"))
+        # wandb.init(settings=wandb.Settings(start_method="fork"))
+        wandb.init(settings=wandb.Settings(start_method="fork"),name=config.get("wandb_name", None), tags=config.get("wandb_tags", None))
         # wandb.init(entity="hierarchical-negative-sampling", project="hns")
         wandb.config.update(config, allow_val_change=True)
         config = wandb.config
