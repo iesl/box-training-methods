1eb95bfb3884734828fadb3a29e9c6128e6b7d29
brozonoyer_umass_edu <brozonoyer@iesl.cs.umass.edu>
2024-01-27 12:10:31 +0000

    increase mem

--- icml2024/3_wordnet/run_eval_on_checkpoint.sh
+++ icml2024/3_wordnet/run_eval_on_checkpoint.sh
@@ -2,12 +2,16 @@
 #SBATCH -c 4
 #SBATCH -p gpu
 #SBATCH --gpus=1
-#SBATCH --mem=16G
+#SBATCH --mem=128G
 #SBATCH -t 7:00:00
 #SBATCH -o /work/pi_mccallum_umass_edu/brozonoyer_umass_edu/box-training-methods/icml2024/3_wordnet/slurm/slurm-%j.out
 
 source /work/pi_mccallum_umass_edu/brozonoyer_umass_edu/anaconda3/bin/activate box-training-methods
 cd /work/pi_mccallum_umass_edu/brozonoyer_umass_edu/box-training-methods/icml2024/3_wordnet/
+
+PARTITION=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "./partition_assignments.txt")
+echo ${PARTITION}
+scontrol update PartitionName=${PARTITION}
 
 MODEL_CKPT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "./model_ckpts.txt")
 MODEL_TYPE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "./model_types.txt")
